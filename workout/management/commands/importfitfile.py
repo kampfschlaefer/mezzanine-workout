0767686a4e26fa46e6a6010eca58f7a123e08be0
Arnold Krille <arnold@arnoldarts.de>
2013-10-04 14:35:50 +0200

    Remove the moved code

--- workout/management/commands/importfitfile.py
+++ workout/management/commands/importfitfile.py
@@ -21,7 +21,7 @@ from django.utils.timezone import get_default_timezone
 from fitparse.base import FitFile
 
 from ...models import Workout, Lap, Record
-from ...helpers import FullDatetimeProcessor
+from ...helpers import FullDatetimeProcessor, importfitfile
 
 class Command(BaseCommand):
     args = '<fitfile> ...'
@@ -33,42 +33,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for f in args:
-            self.logger.info(u"Processing '%s'..." % f)
-            fitfile = FitFile(
-                f,
-                data_processor=FullDatetimeProcessor(),
-                check_crc=False
-            )
-            workout = Workout()
-            self.logger.info(u"Processing session")
-            for msg in fitfile.get_messages(name='session', as_dict=True):
-                self.logger.debug(u"Session fields: %s" % str(msg['fields']))
-                for f in msg['fields']:
-                    if f['name'] == 'timestamp':
-                        workout.title = f['value'].astimezone(get_default_timezone()).isoformat()
-                    if f['name'] == 'start_time':
-                        workout.publish_date = f['value']
-                    if hasattr(workout, f['name']):
-                        setattr(workout, f['name'], f['value'])
-
-            workout.user = get_user_model().objects.all()[0]
-            workout.save()
-
-            self.logger.info(u"Processing laps")
-            for msg in fitfile.get_messages(name='lap', as_dict=True):
-                self.logger.info(" Lap: '%s'" % msg['name'])
-                lap = Lap(workout=workout)
-                for f in msg['fields']:
-                    if hasattr(lap, f['name']):
-                        setattr(lap, f['name'], f['value'])
-                lap.save()
-
-            self.logger.info(u"Processing data points")
-            for msg in fitfile.get_messages(name='record', as_dict=True):
-                record = Record(workout=workout)
-                for f in msg['fields']:
-                    if hasattr(record, f['name']):
-                        setattr(record, f['name'], f['value'])
-                record.save()
-
-            self.logger.info("Finished.")
+            importfitfile(f)
